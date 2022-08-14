ce059285dd9d3ba2aa1db4ab47cec86fe23ba388
Sanket <kajaresanket21@gmail.com>
2022-08-14 18:45:47 +0530

    First commit

---
+++ server/config/dev.js
@@ -0,0 +1,6 @@
+module.exports={
+    MONGOURI:"mongodb+srv://Sanchita:$anchita@123@cluster0.w5gee.mongodb.net/test?retryWrites=true&w=majority",
+    JWT_SECRET:"abcsefghij",
+    SENDGRID_API:process.env.SENDGRID_API,
+    EMAIL:process.env.EMAIL
+}
